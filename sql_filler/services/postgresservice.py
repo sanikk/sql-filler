409e58c5fab2ea55898dd8568dba7b07804e8a42
sanikk <sanikk@users.noreply.github.com>
2024-04-02 20:01:19 +0300

    bare bones postgresservice

---
+++ sql_filler/services/postgresservice.py
@@ -0,0 +1,25 @@
+from sql_filler.db_module import test_connection
+
+
+class PostgresService:
+
+    def __init__(self):
+        self.__dbname = None
+        self.__username = None
+
+    def __connect(self, dbname: str, username: str):
+        self.__dbname = dbname
+        self.__username = username
+
+    def __disconnect(self):
+        self.__dbname = None
+        self.__username = None
+
+    def try_connection(self, dbname: str, username: str) -> bool:
+        if test_connection(dbname, username):
+            self.__connect(dbname, username)
+            return True
+        return False
+
+    def is_connected(self):
+        return self.__dbname is not None and self.__username is not None
