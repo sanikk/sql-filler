759c90dcdec63a88bb617134a6c4fef920950532
sanikk <sanikk@users.noreply.github.com>
2024-04-05 17:03:48 +0300

    cleaned up db_connection

--- sql_filler/db_connection.py
+++ sql_filler/db_connection.py
@@ -1,24 +1,30 @@
 from psycopg2 import connect, OperationalError
-from sqlalchemy import text
 from dotenv import load_dotenv
-from os import getenv
 
 
 load_dotenv()
 
 
-def stupid_mock_checker(input:str):
-    for merkki in input:
-        if merkki.isdigit() and
-            merkki.isalpha() or
-            merkki == '_':
-            return True
-    return False
+def stupid_connection_string_checker(input_string: str):
+    """
+    Checks if the input_string has only letters and digits and underscores.
+    :param input_string:
+    :return:
+    """
+    if not input_string:
+        return False
+    for character in input_string:
+        # These should be alphabets, digits or underscores
+        if character.isdigit() or character.isalpha() or character == '_':
+            continue
+        return False
+    return True
+
 
 def get_connection(dbname, username):
-    username = text
+    if stupid_connection_string_checker(dbname) or stupid_connection_string_checker(username):
+        return None
     try:
-        # TODO sanitize this ASAP!
         conn = connect(f"dbname={dbname} user={username}")
     except OperationalError:
         # TODO log this
