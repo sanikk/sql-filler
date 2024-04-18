734cd0af5a096cc13ad705946e5d24e83006e82e
sanikk <sanikk@users.noreply.github.com>
2024-04-18 15:51:53 +0300

    scrollbar works. limits dont, but still. cleanup needed

--- sql_filler/ui/insertTab.py
+++ sql_filler/ui/insertTab.py
@@ -1,12 +1,11 @@
 from tkinter.ttk import Entry, Button, Treeview, Frame, Scrollbar
 from tkinter import Canvas
-from sql_filler.ui.utils import get_container
 
 
 class InsertTab:
     def __init__(self, master=None, ui=None):
         self.frame = Frame(master=master)
-        self.frame.rowconfigure(1, weight=0)
+        self.frame.rowconfigure(1, weight=1)
 
         self.scrollbar = Scrollbar(master=self.frame)
         self.scrollable = Canvas(master=self.frame, yscrollcommand=self.scrollbar.set)
@@ -14,21 +13,30 @@ class InsertTab:
         self.scrollable.grid(row=1, column=1)
         self.scrollbar.grid(row=1, column=2, sticky='NS')
 
-        self.scrollable.rowconfigure(1, weight=1)
+        self.scrollable.rowconfigure(1, weight=0)
         # self.frame.rowconfigure(1, weight=1)
 
         self.box_container = Frame(master=self.scrollable)
         self.box_container.columnconfigure(0, weight=3) # big/small button
         self.box_container.columnconfigure(1, weight=1) # entry box
-        self.box_container.grid(row=1, column=0)
+        # self.box_container.grid(row=1, column=0)
+        self.scrollable.create_window((0,0), window=self.box_container, anchor='nw')
+        self.box_container.bind("<Map>", self._reset_scrollregion())
+        # self.box_container.
 
         # Button(master=self.frame, text="Insert to DB", command=self.insert_values).grid(row=1, column=0)
 
         self._ui = ui
-        self._entry_boxes = {}
+        self._entry_boxes = []
+        # wrong place for this i think
+        self._generated_values = []
 
     def populate_insert_columns_tab(self):
         column_list = self._ui.get_insert_tab()
+        if not column_list:
+            print("No columns")
+            print(f"{self._ui.get_selected_table()}")
+            return
         for column_data in column_list:
             self.make_single_row(column_data=column_data)
 
@@ -38,8 +46,19 @@ class InsertTab:
         self.populate_insert_columns_tab()
         self._reset_scrollregion()
 
+    def collect_values(self):
+        values = [(i, box.get()) for i, box in self._entry_boxes]
+        return values
+
+    def generate_values(self):
+        values = self.collect_values()
+        self._ui.generate_values(values)
+
     def insert_values(self):
-        pass
+        self._ui.insert_generated_values()
+
+    def discard_generated_values(self):
+        self._generated_values.clear()
 
     def show_generated_values(self, master=None, column_names=None, data=None):
         pass
@@ -47,16 +66,16 @@ class InsertTab:
 
     def get_frame(self):
         """
-        This one is need now for tab_switcher(ttk.Notebook) when adding the tab.
+        This one is needed now for tab_switcher(ttk.Notebook) when adding the tab.
 
         :return:
         """
         return self.frame
 
     def make_single_row(self, master=None, column_data=None):
+
         if not master:
             master = self.box_container
-
         ordinal_position = column_data['ordinal_position']
 
         def expand():
@@ -76,11 +95,12 @@ class InsertTab:
         bigbutton = Button(master=master, text=txt, command=shrink)
         valbox = Entry(self.box_container, width=20)
         valbox.grid(row=ordinal_position, column=1)
-        self._entry_boxes[ordinal_position] = valbox
+        self._entry_boxes.append((ordinal_position, valbox))
 
     def _reset_scrollregion(self):
         # might need the bbox one too
         self.box_container.update()
         self.frame.update()
-        self.scrollable.config(scrollregion=self.scrollable.bbox('all'))
+        # self.scrollbar.
+        # self.box_container.config(scrollregion=self.scrollable.bbox('all'))
 
