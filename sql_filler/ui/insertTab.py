61359db2de2b529f8c0d036058ea6ef11d2cb8a0
sanikk <sanikk@users.noreply.github.com>
2024-04-19 11:54:35 +0300

    inserttab: layout, datatype, organization

--- sql_filler/ui/insertTab.py
+++ sql_filler/ui/insertTab.py
@@ -16,16 +16,19 @@ class InsertTab:
         self.scrollbar.config(command=self.scrollable.yview)
         self.scrollable.grid(row=1, column=0, sticky='NS')
         self.scrollbar.grid(row=1, column=1, sticky='NS')
-
         self.scrollable.rowconfigure(1, weight=0)
 
+        self.generate_button = Button(master=self.frame, text="Generate inserts",
+                                      command=self.generate_insert_statements)
+        self.generate_button.grid(row=1, column=2)
+
         self.box_container = Frame(master=self.scrollable)
-        self.box_container.columnconfigure(0, weight=3) # big/small button
+        self.box_container.columnconfigure(0, weight=1) # big/small button
         self.box_container.columnconfigure(1, weight=1) # entry box
-        # self.box_container.grid(row=1, column=0)
+        self.box_container.columnconfigure(2, weight=1)  # entry box
+
         self.scrollable.create_window((0, 0), window=self.box_container, anchor='nw')
         self.box_container.bind("<Map>", self._reset_scrollregion())
-        # self.box_container.
 
         # Button(master=self.frame, text="Insert to DB", command=self.insert_values).grid(row=1, column=0)
 
@@ -36,6 +39,7 @@ class InsertTab:
         column_list = self._ui.get_insert_tab()
         if not column_list:
             return
+        self._entry_boxes.clear()
         for column_data in column_list:
             self.make_single_row(column_data=column_data)
 
@@ -46,18 +50,19 @@ class InsertTab:
         self._reset_scrollregion()
 
     def _collect_values(self):
-        values = [(i, box.get()) for i, box in self._entry_boxes]
+        print(f"{self._entry_boxes}")
+        values = [box.get() for box in self._entry_boxes]
         return values
 
-    def generate_values(self):
+    def generate_insert_statements(self):
         values = self._collect_values()
-        self._ui.generate_values(values)
+        self._ui.generate_insert_statements(values)
 
     def insert_values(self):
-        self._ui.insert_generated_values()
+        pass
 
     def discard_generated_values(self):
-        self._generated_values.clear()
+        pass
 
     def show_generated_values(self, master=None, column_names=None, data=None):
         pass
@@ -72,29 +77,34 @@ class InsertTab:
         return self.frame
 
     def make_single_row(self, master=None, column_data=None):
-
         if not master:
             master = self.box_container
         ordinal_position = column_data['ordinal_position']
 
+        # small button/datatype label area (replaces big button)
         def expand():
             smallbutton.grid_forget()
-            bigbutton.grid(row=ordinal_position, column=0)
+            datatype_label.grid_forget()
+            bigbutton.grid(row=ordinal_position, column=0, columnspan=2)
             self._reset_scrollregion()
-
+        datatype_label = Label(master=master, text=column_data["data_type"])
         smallbutton = Button(master=master, text=column_data["column_name"], command=expand)
         smallbutton.grid(row=ordinal_position, column=0)
+        datatype_label.grid(row=ordinal_position, column=1)
 
+        # big button area (replaces small button/da..)
         def shrink():
             bigbutton.grid_forget()
             smallbutton.grid(row=ordinal_position, column=0)
+            datatype_label.grid(row=ordinal_position, column=1)
             self._reset_scrollregion()
-
         txt = '\n'.join([f"{key}: {column_data[key]}" for key in column_data.keys()])
         bigbutton = Button(master=master, text=txt, command=shrink)
+
+        # entry box area
         valbox = Entry(self.box_container, width=20)
-        valbox.grid(row=ordinal_position, column=1)
-        self._entry_boxes.append((ordinal_position, valbox))
+        valbox.grid(row=ordinal_position, column=2)
+        self._entry_boxes.append(valbox)
 
     def _reset_scrollregion(self):
         # might need the bbox one too
