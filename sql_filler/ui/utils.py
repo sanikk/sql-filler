d3b5c03843e8a927f91226ac02f4f5a0b0dd303f
sanikk <sanikk@users.noreply.github.com>
2024-04-27 17:31:21 +0300

    moving make_scrollable_frame to util

--- sql_filler/ui/utils.py
+++ sql_filler/ui/utils.py
@@ -1,4 +1,5 @@
-from tkinter import Label, Frame
+from tkinter import Canvas, Label, Frame
+from tkinter.ttk import Scrollbar
 
 
 def get_container(master=None, text=None, columnspan=1, rowspan=1):
@@ -13,4 +14,26 @@ def get_main_label(master=None):
     main_label = Label(master=master, text='SQL Filler', fg='purple', font='Calibri 24')
     return main_label
 
+
+def make_scrollable_frame(master=None):
+    """
+    Makes a scrollable frame. Self contained function, just needs the parent window as param.
+
+    :param master:
+    :return: scrollable(tk.Canvas), box_container(Frame)
+    """
+    scrollbar = Scrollbar(master=master)
+    scrollable = Canvas(master=master, yscrollcommand=scrollbar.set)
+    scrollbar.config(command=scrollable.yview)
+    scrollable.grid(row=2, column=0, sticky='NSEW')
+    scrollbar.grid(row=2, column=1, sticky='NS')
+    scrollable.rowconfigure(1, weight=0)
+
+    box_container = Frame(master=scrollable)
+    box_container.columnconfigure(0, weight=1)  # big/small button
+    box_container.columnconfigure(1, weight=1)  # datatype label
+    box_container.columnconfigure(2, weight=1)  # entry box
+
+    scrollable.create_window((0, 0), window=box_container, anchor='nw')
+    return scrollable, box_container
 
