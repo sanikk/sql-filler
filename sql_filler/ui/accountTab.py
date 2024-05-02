ec649bbcaf126c951b7601878c2566797db3533f
sanikk <sanikk@users.noreply.github.com>
2024-05-03 00:24:05 +0300

    not yet good

--- sql_filler/ui/accountTab.py
+++ sql_filler/ui/accountTab.py
@@ -1,70 +1,47 @@
-from tkinter import Label, Entry, Button, Frame, EW
-from sql_filler.ui.utils import get_container
+from tkinter.ttk import Label, Entry, Button, Frame, LabelFrame
 
 
 class AccountTab:
     # we need filler
     # https://www.postgresql.org/docs/9.0/functions-info.html
-    def __init__(self, master=None, data_service=None, table=None):
+    def __init__(self, master=None, data_service=None, ui=None):
         self.frame = Frame(master=master)
-        self._connection_tab_position_params = {'row': 1, 'column': 1, 'sticky': 'EW'}
-
-        self._dbname_entry_box = None
         self._username_entry_box = None
+        self._database_entry_box = None
+        self.unconnected_box = self.unconnected(master=self.frame)
+        self.unconnected_box.grid(row=1, column=0, rowspan=2)
 
-        self._unconnected_tab = self.unconnected_tab()
-        self._unconnected_tab.grid(self._connection_tab_position_params)
-        self._connected_tab = None
+        Button(master=self.frame, text="Connect", command=self.connect).grid(row=1, column=1, sticky='EW')
+        self.disconnector = Button(master=self.frame, text="Disconnect", command=self.disconnect,
+                                   state='disabled')
+
+        self.disconnector.grid(row=2, column=1, sticky='EW')
 
         self._data_service = data_service
-        self._table = table
+        self._ui = ui
+
+    def unconnected(self, master=None):
+        connected_box = LabelFrame(master=self.frame)
+        Label(master=connected_box, text="Username").grid(row=1, column=0)
+        self._username_entry_box = Entry(master=connected_box)
+        self._username_entry_box.grid(row=2, column=0)
+
+        Label(master=connected_box, text="Database name").grid(row=3, column=0)
+        self._dbname_entry_box = Entry(master=connected_box)
+        self._dbname_entry_box.grid(row=4, column=0)
+        return connected_box
 
     def connect(self):
-        # dbname, username = self.get_entry_values()
-        dbname, username = 'ravintolasovellus', 'karpo'
-        if dbname and username and self._data_service.account_new(dbname, username):
-            self._connected_tab = self.connected_tab()
-            self._unconnected_tab.grid_forget()
-            self._connected_tab.grid(self._connection_tab_position_params)
+        dbname, username = self.get_entry_values()
 
-            self._table.update_tables()
+        if dbname and username and self._data_service.account_new(dbname, username):
+            self._ui.set_tabs_state('normal')
+            self.disconnector.configure(state='normal')
 
     def disconnect(self):
         self._data_service.account_close()
-        self._connected_tab.destroy()
-        self._unconnected_tab.grid(self._connection_tab_position_params)
-
-        self._table.update_tables()
-
-    def unconnected_tab(self, master=None) -> Frame:
-        container = self.get_tab()
-
-        Label(master=container, text="Username").grid(row=1, column=0)
-        self._username_entry_box = Entry(master=container)
-        self._username_entry_box.grid(row=2, column=0)
-
-        Label(master=container, text="Database name").grid(row=3, column=0)
-        self._dbname_entry_box = Entry(master=container)
-        self._dbname_entry_box.grid(row=4, column=0)
-
-        Button(master=container, text="Connect", command=self.connect).grid(row=1, column=1, rowspan=4, sticky=EW)
-
-        return container
-
-    def connected_tab(self, master=None) -> Frame:
-        container = self.get_tab(text='Connected to')
-
-        dbname, username = self._data_service.account_get_login_info()
-        Label(master=container, text=f'{username}@{dbname}').grid(row=1, column=0, sticky=EW)
-
-        Button(master=container, text="Disconnect", command=self.disconnect).grid(row=2, column=0, sticky=EW)
-
-        return container
-
-    def get_tab(self, **kwargs):
-        container = get_container(master=self.frame)
-        container.grid(self._connection_tab_position_params, sticky=EW)
-        return container
+        self._ui.set_tabs_state('disabled')
+        self.disconnector.configure(state='disabled')
 
     def grid(self, row, column, sticky):
         """
--- sql_filler/ui/ui.py
+++ sql_filler/ui/ui.py
@@ -5,9 +5,6 @@ import tkinter.ttk as ttk
 from sql_filler.ui.accountTab import AccountTab
 from sql_filler.ui.insertTab import InsertTab
 from sql_filler.ui.statementTab import StatementTab
-from sql_filler.ui.accountFrame import AccountFrame
-from sql_filler.ui.tableFrame import TableFrame
-from sql_filler.ui.workFrame import WorkFrame
 
 from sql_filler.ui.utils import get_main_label
 
@@ -21,32 +18,33 @@ class UI:
         self._data_service = data_service
 
         self._main_label = get_main_label(master=self.frame)
-        self._main_label.grid(row=0, column=0, columnspan=2)
+        self._main_label.grid(row=0, column=0, columnspan=1)
 
-        self.menu = self.get_menu_bar(self.frame)
+        self.menu = self.get_menu_bar(master=self.frame, data_service=data_service)
         self.menu.grid(row=1, column=0)
+        self.frame.rowconfigure(1, weight=1)
+        self.frame.columnconfigure(0, weight=1)
 
+        self.insert_tab = None
         self.account_tab = None
         self.statement_tab = None
-
-        # self._work = WorkFrame(master=self.frame, data_service=data_service)
-        # self._table = TableFrame(master=self.frame, data_service=data_service, work=self._work)
-        # self._account = AccountFrame(master=self.frame, data_service=data_service, table=self._table)
 
         # self._style()
         # self._grid()
         # self._layout()
 
     def get_menu_bar(self, master=None, data_service=None):
+
         tab_switcher = ttk.Notebook(master=master)
-        self.account_tab = AccountTab(master=master, data_service=data_service)
+
+        self.account_tab = AccountTab(master=master, data_service=data_service, ui=self)
         tab_switcher.add(self.account_tab.get_frame(), text='account tab')
 
         self.insert_tab = InsertTab(master=master, data_service=data_service)
-        tab_switcher.add(self.insert_tab.get_frame(), text='column data tab')
+        tab_switcher.add(self.insert_tab.get_frame(), text='column data tab', state='disabled')
 
         self.statement_tab = StatementTab(master=master, data_service=data_service)
-        tab_switcher.add(self.statement_tab.get_frame(), text='statements tab')
+        tab_switcher.add(self.statement_tab.get_frame(), text='statements tab', state='disabled')
 
         # tab_switcher.add(self.db_info_tab(master=master), text='db info')
 
@@ -54,47 +52,49 @@ class UI:
 
         return tab_switcher
 
+    def set_tabs_state(self, state):
+        for tab in self.menu.tabs()[1:]:
+            self.menu.tab(tab, state=state)
 
-
-    def _style(self):
-        # TODO not doing anything really just now
-        app_style = ttk.Style()
-        app_style.theme_use('clam')
-        app_style.configure('Container.TFrame', borderwidth=5)
-        # app_style.configure('border', borderwidth=5)
-        # app_style.configure('focus', focuscolor='yellow', focusthickness=5)
-        # app_style.configure('.', font='Symbols Nerd Font')
-        # app_style.configure('.', font='Source Code Pro')
-
-    def _grid(self):
-        self._main_label.grid(row=0, column=0, columnspan=2)
-        self._account.grid(row=1, column=0, sticky=tk.E+tk.W+tk.N+tk.S)
-        self._table.grid(row=2, column=0, sticky=tk.N+tk.E)
-        self._work.grid(row=1, column=1, rowspan=2)
-
-    def _layout(self):
-        self.frame.columnconfigure(index=0, weight=0)
-        self.frame.columnconfigure(index=1, weight=0)
-        self.frame.rowconfigure(0, weight=0)
-        self.frame.rowconfigure(index=1, weight=0)
-        self.frame.rowconfigure(index=2, weight=0)
-
-    def generate_insert_statements(self, table_number, amount, base_strings):
-        """
-        This should be useful here. We need to update another frame in workFrame.
-
-        """
-        return self._data_service.generate_insert_statements(table_number=table_number, amount=amount,
-                                                             base_strings=base_strings)
-
-    def discard_generated_values(self):
-        """
-        Probably not needed here. Put this in the generated_values tab.
-
-        :return:
-        """
-        self._data_service.discard_generated_values()
-        self._work.discard_generated_values()
+    # def _style(self):
+    #     # TODO not doing anything really just now
+    #     app_style = ttk.Style()
+    #     app_style.theme_use('clam')
+    #     app_style.configure('Container.TFrame', borderwidth=5)
+    #     # app_style.configure('border', borderwidth=5)
+    #     # app_style.configure('focus', focuscolor='yellow', focusthickness=5)
+    #     # app_style.configure('.', font='Symbols Nerd Font')
+    #     # app_style.configure('.', font='Source Code Pro')
+    #
+    # def _grid(self):
+    #     self._main_label.grid(row=0, column=0, columnspan=2)
+    #     self._account.grid(row=1, column=0, sticky=tk.E+tk.W+tk.N+tk.S)
+    #     self._table.grid(row=2, column=0, sticky=tk.N+tk.E)
+    #     self._work.grid(row=1, column=1, rowspan=2)
+    #
+    # def _layout(self):
+    #     self.frame.columnconfigure(index=0, weight=0)
+    #     self.frame.columnconfigure(index=1, weight=0)
+    #     self.frame.rowconfigure(0, weight=0)
+    #     self.frame.rowconfigure(index=1, weight=0)
+    #     self.frame.rowconfigure(index=2, weight=0)
+    #
+    # def generate_insert_statements(self, table_number, amount, base_strings):
+    #     """
+    #     This should be useful here. We need to update another frame in workFrame.
+    #
+    #     """
+    #     return self._data_service.generate_insert_statements(table_number=table_number, amount=amount,
+    #                                                          base_strings=base_strings)
+    #
+    # def discard_generated_values(self):
+    #     """
+    #     Probably not needed here. Put this in the generated_values tab.
+    #
+    #     :return:
+    #     """
+    #     self._data_service.discard_generated_values()
+    #     self._work.discard_generated_values()
 
     # passthrough function for main.py so far
     def mainloop(self):
